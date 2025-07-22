233e84201b96e0ea0d8512998dbe4e9130f2aa6b
pritam Kumar <pritamkumar159Pro@gmail.com>
2025-07-22 13:45:55 +0530

    state final complete changes

--- backend/controllers/common.controller.js
+++ backend/controllers/common.controller.js
@@ -1,13 +1,20 @@
 import Country from "../models/Country.model.js";
+import State from "../models/state.model.js";
 export const getCounts = async (req, res) => {
   const counts = {};
   try {
-    const [countryCount] = await Promise.all([Country.countDocuments()]);
+    const [countryCount, stateCount] = await Promise.all([
+      Country.countDocuments(),
+      State.countDocuments(),
+    ]);
     counts.country = countryCount;
     res.status(200).json({
       success: true,
       message: "count retrieved successfully",
-      data: counts,
+      data: {
+        countryCount,
+        stateCount,
+      },
     });
   } catch (error) {
     res.status(500).json({
