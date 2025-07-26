bd3930ac57c8bbe7823a2bc5d017cc7928ea1a56
pritam Kumar <pritamkumar159Pro@gmail.com>
2025-07-26 15:53:43 +0530

    some changes

--- frontend/components/footer.php
+++ frontend/components/footer.php
@@ -1,6 +1,20 @@
-<!-- Footer Component -->
-<div class="footer">
-  Copyright &copy; 2025 <a href="#">Shish Technology</a>. All rights
-  reserved.
-  <span style="float: right; color: #aaa">Version 1.1.0</span>
-</div>
+<!-- Compact Modern Footer -->
+<footer class="footer">
+  <div class="footer-row">
+    <div class="footer-brand">
+      <i class="fa fa-graduation-cap"></i>
+      <span>Shish Technology</span>
+    </div>
+    <div class="footer-copyright">
+      &copy; 2025 <strong>Shish Technology</strong>. All rights reserved.
+    </div>
+    <div class="footer-meta">
+      <span class="footer-version">v1.1.0</span>
+      <span class="footer-social">
+        <a href="#" class="social-link" title="Facebook"><i class="fab fa-facebook-f"></i></a>
+        <a href="#" class="social-link" title="Twitter"><i class="fab fa-twitter"></i></a>
+        <a href="#" class="social-link" title="LinkedIn"><i class="fab fa-linkedin-in"></i></a>
+      </span>
+    </div>
+  </div>
+</footer>
